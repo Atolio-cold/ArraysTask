8a456b566bd167c6b9e5c9787bab15fd967d36b7
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-09 21:23:14 +0100

    Add files via upload

---
+++ Arrays.cs
@@ -0,0 +1,41 @@
+// using System;
+
+// class Array
+// {
+//     static void Main()
+//     {
+//         int[] array1 = new int[5];
+//         string[] Car = { "BMW", "Toyota", "ASTRA", "MERCEDES", "OPEL" };
+
+//         Car[2] = "PASAT";
+
+//         for (int i = 0; i < Car.Length; i++)
+//         {
+
+//             Console.WriteLine("Car" + (i + 1) + " " + Car[i]);
+//         }
+
+//         int[] numbers = { 1, 2, 3, 4, 5 };
+
+//         // Dostęp do elementów tablicy
+//         Console.WriteLine("Pierwszy element: " + numbers[0]); // 1
+//         Console.WriteLine("Drugi element: " + numbers[1]);  // 2
+
+//         // Zmiana wartości w tablicy
+//         numbers[2] = 10; // Zmieniamy trzeci element na 10
+//         Console.WriteLine("Trzeci element po zmianie: " + numbers[2]); // 10
+//         numbers[3] = 20;
+//         Console.WriteLine("Czwarta liczba" + numbers[3]);
+
+//         // Wyświetlenie całej tablicy
+//         Console.WriteLine("\nCała tablica:");
+//         for (int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.WriteLine("Element " + i + ": " + numbers[i]);
+//         }
+//     }
+
+// }
+
+
+
