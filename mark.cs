6644ab01964b7c02c60d36669807ea51c04bf2dd
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-10 16:51:26 +0100

    Update mark.cs

--- mark.cs
+++ mark.cs
@@ -10,30 +10,24 @@ class Program
 
         int najlepszaOcena = oceny[0];
 
-        // Zmienna do przechowywania sumy ocen
         int suma = 0;
 
         Array.Sort(oceny);
 
-        // Zmienna do przechowywania mediany
         double mediana;
 
-        // Sprawdzenie, czy liczba elementów jest parzysta czy nieparzysta
         if (oceny.Length % 2 == 0)
         {
-            // Jeśli parzysta, mediana to średnia dwóch środkowych elementów
             int midIndex1 = oceny.Length / 2 - 1;
             int midIndex2 = oceny.Length / 2;
             mediana = (oceny[midIndex1] + oceny[midIndex2]) / 2.0;
         }
         else
         {
-            // Jeśli nieparzysta, mediana to środkowy element
             int midIndex = oceny.Length / 2;
             mediana = oceny[midIndex];
         }
 
-        // Wyświetlenie wyników
         Console.WriteLine("Posortowane oceny ucznia: ");
         for (int i = 0; i < oceny.Length; i++)
         {
